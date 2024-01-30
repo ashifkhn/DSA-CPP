42f352785740b904c25f0eda67f3505e6a4292fc
Ashif Khan <ashifkhn02@gmail.com>
2024-01-30 20:02:10 +0530

    Find unique with XOR^

--- Arrays/1SwapAlternate.cpp
+++ Arrays/1SwapAlternate.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
 using namespace std;
-
 void swapAlternate(int arr[],int size){
-    for (int i = 0; i < size;i=i+2){
+    int temp;
+    for (int i = 0; i < size; i = i + 2)
+    {
         if(i+1<size){
-        swap(arr[i],arr[i+1]);
-    }
+        // swap(arr[i],arr[i+1]);
+        temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        }
     }
 }
-
 void printArray(int arr[],int size){
     for (int i = 0; i < size;i++){
         cout<<arr[i]<<" ";
     }
     cout<<endl;
 }
-
 int main(){
-    int arr[6] = {1, 2, 3, 4, 5,6};
-    swapAlternate(arr,6);
-    printArray(arr,6);
+    int arr[7] = {1, 2, 3, 4, 5,6,9};
+    swapAlternate(arr,7);
+    printArray(arr,7);
 }
---
+++ Arrays/FindUnique.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int findUnique(int arr[],int size){
+    int ans = 0;
+    for (int i = 0; i < size; i++)
+    {
+        ans = ans ^ arr[i];
+    }
+    return ans;
+}
+
+int main(){
+
+    int arr[9] = {1, 1, 2, 2, 3, 3, 4,4,5};
+    int unique = findUnique(arr, 9);
+    cout << unique << endl;
+}
